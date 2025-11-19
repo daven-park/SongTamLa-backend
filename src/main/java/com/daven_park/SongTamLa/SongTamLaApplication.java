3cf76a7bd734b1bf5acc6c1534fe023411bc5f96
ChangHyun Park <buschanny@gmail.com>
2025-11-19 18:56:45 +0900

    chore : backend init setting

---
+++ src/main/java/com/daven_park/SongTamLa/SongTamLaApplication.java
@@ -0,0 +1,13 @@
+package com.daven_park.SongTamLa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongTamLaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongTamLaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daven_park/SongTamLa/SongTamLaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daven_park.SongTamLa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongTamLaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
